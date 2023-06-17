0f0a2c06f5271b6f13dc12651d406f1e718ada00
ihor-vip <vip20140902@gmail.com>
2023-06-17 16:28:46 +0300

    create music player and implement playing songs from different components

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -2,12 +2,15 @@
 
 import { Song } from "@/types";
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProps {
     songs: Song[];
 }
 
 const PageContent: React.FC<PageContentProps> = ({songs}) => {
+    const onPlay = useOnPlay(songs);
+
     if (songs.length === 0) {
         return (
             <div className='mt-4 text-neutral-400'>
@@ -32,7 +35,7 @@ const PageContent: React.FC<PageContentProps> = ({songs}) => {
         >
             {songs.map((item) => (
                 <SongItem
-                    onClick={() => {}}
+                    onClick={(id: string) => onPlay(id)}
                     key={item.id}
                     data={item}
                 />
