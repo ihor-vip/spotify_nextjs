fd5c16669f58ed95bec2a9c5849530be39ef27bf
ihor-vip <vip20140902@gmail.com>
2023-06-19 12:37:21 +0300

    small update @MediaItem.tsx

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { Song } from "@/types";
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
     data: Song;
@@ -11,12 +12,15 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({data, onClick}) => {
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
 
     const handleClick = () => {
         if (onClick) {
             return onClick(data.id);
         }
+
+        return player.setId(data.id);
     };
 
     return (
